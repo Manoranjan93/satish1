0a1fc7e95765667a3250188ced240610389a2b69
matrunibedit <matrunibedit@gmail.com>
2019-04-24 12:07:03 +0530

    Line 7 WorkSpace2

--- src/Japp.java
+++ src/Japp.java
@@ -4,6 +4,7 @@ public class Japp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          System.out.println("Hello Got World");
+         System.out.println("Line 7 Workspace2");
 	}
 
 }
